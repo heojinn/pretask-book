b9acbb452f94481ced883492f6cdce97c5962478
Jin <hurjin1109@naver.com>
2026-08-22 22:11:59 +0900

    init

---
+++ src/test/java/com/example/pretask/PretaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pretask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PretaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
